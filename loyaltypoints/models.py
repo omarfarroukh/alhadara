f9e009e596b22df900e086609a91028ae4f05010
omarfarroukh <omarfarroukh2002@gmail.com>
2025-07-20 14:42:03 +0300

    got rid of useless code

--- loyaltypoints/models.py
+++ loyaltypoints/models.py
@@ -8,19 +8,3 @@ class LoyaltyPoint(models.Model):
 
     def __str__(self):
         return f"{self.student} - {self.points} points"
-
-    def transform_to_ewallet(self, amount, reason="Transformed to ewallet"):
-        if self.points < amount:
-            raise ValueError("Not enough points")
-        self.points -= amount
-        # Assume student has 'ewallet_balance' field
-        self.student.ewallet_balance = getattr(self.student, 'ewallet_balance', 0) + amount
-        self.student.save()
-        self.save()
-        LoyaltyPointAudit.objects.create(
-            student=self.student,
-            change=-amount,
-            reason=reason,
-            balance_after=self.points
-        )
-
